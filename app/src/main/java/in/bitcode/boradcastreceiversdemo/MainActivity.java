0448701bee50fb901da267a6124ac282d24d1441
vishaljagtap3 <vishal.jagtap@bitcode.in>
2020-11-06 22:43:12 +0530

    Broadcast 1

--- app/src/main/java/in/bitcode/boradcastreceiversdemo/MainActivity.java
+++ app/src/main/java/in/bitcode/boradcastreceiversdemo/MainActivity.java
@@ -2,13 +2,58 @@ package in.bitcode.boradcastreceiversdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnRegister, mBtnUnregister, mBtnSendBroadcast;
+    private MyBR mMyBR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnRegister = findViewById(R.id.btnRegister);
+        mBtnUnregister = findViewById(R.id.btnUnRegister);
+        mBtnSendBroadcast = findViewById(R.id.btnSendBroadcast);
+
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMyBR = new MyBR();
+
+                IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_LOW);
+                intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+                intentFilter.addAction(Intent.ACTION_WALLPAPER_CHANGED);
+                intentFilter.addAction(Intent.ACTION_HEADSET_PLUG);
+                intentFilter.addAction("in.bitcode.course.download.COMPLETE");
+
+                registerReceiver(mMyBR, intentFilter);
+
+            }
+        });
+        mBtnUnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unregisterReceiver(mMyBR);
+            }
+        });
+
+        mBtnSendBroadcast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("in.bitcode.course.download.COMPLETE");
+                //intent.setAction(Intent.ACTION_BATTERY_LOW);
+                intent.putExtra("path", "/storage/bitcode/android/tutorial.mp4");
+                sendBroadcast(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/in/bitcode/boradcastreceiversdemo/MyBR.java
+++ app/src/main/java/in/bitcode/boradcastreceiversdemo/MyBR.java
@@ -9,6 +9,23 @@ public class MyBR extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, intent.getAction(), Toast.LENGTH_LONG).show();
+
+        if( intent.getAction().equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)) {
+            boolean isEnabled = intent.getBooleanExtra("state", false);
+            String message = isEnabled ? "Airplane mode is turned ON!" : "Airplane mode is turned Off!";
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+        else {
+            if(intent.getAction().equals("in.bitcode.course.download.COMPLETE")) {
+                Toast.makeText(
+                        context,
+                        intent.getAction() + "\n" + intent.getStringExtra("path"),
+                        Toast.LENGTH_LONG
+                ).show();
+            }
+            else {
+                Toast.makeText(context, intent.getAction(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
